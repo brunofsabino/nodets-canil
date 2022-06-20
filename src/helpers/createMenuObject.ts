80c77931f321376e9df0e4cdd76d4f9f48cc3edf
brunofsabino <brunoferrazsabino@gmail.com>
2022-06-19 21:00:50 -0300

    ajustes

--- src/helpers/createMenuObject.ts
+++ src/helpers/createMenuObject.ts
@@ -3,7 +3,7 @@ type MenuOptions = '' | 'all' | 'dog' | 'cat' | 'fish'
 export const createMenuObject = (activeMenu: MenuOptions) => {
     let returnObjetc = {
         all: false,
-        dog: false,
+        dog: false, 
         cat: false,
         fish: false
     }
